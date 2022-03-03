31d7b4f6c6d2308dee92185a88a3c60e16be6803
Hermine <herminemavagyan@gmail.com>
2022-03-03 13:03:31 -0800

    Limit is defined to get the first 807 pokemons

--- React/ReactRouting/pokemonapiassignmentpractice/src/components/Pokemon.js
+++ React/ReactRouting/pokemonapiassignmentpractice/src/components/Pokemon.js
@@ -4,7 +4,7 @@ const Pokemon = (props)=>{
     const [pokemon, setPokemon] = useState([]);
 
     useEffect(() => {
-        fetch('https://pokeapi.co/api/v2/pokemon/')
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=807&offset=0')
             .then(response => response.json())
             .then(response => setPokemon(response.results))
     }, []);
