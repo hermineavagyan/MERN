70b022aaf46a6c625778cb8569d26d8d6f277417
Hermine <herminemavagyan@gmail.com>
2022-03-28 23:28:18 -0700

    button incrmenter is added

--- React/biginversion/src/components/PersonCard.js
+++ React/biginversion/src/components/PersonCard.js
@@ -1,20 +1,27 @@
 import React, { Component }from 'react';
 
 class PersonCard extends Component {
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            age: this.props.age
+        }
+    }
+    // ageIncrementer = ()=>{
+    //     this.setState(this.state.age + 1);
+    // }
     render(){
         const {firstName, lastName, age, hairColor} = this.props;
         return(
             <div>
                 <h3>{lastName}, {firstName}</h3>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair color: {hairColor}</p>
+                <p><button onClick={()=>this.setState({age: this.state.age + 1})}>Birthday button for {firstName} {lastName}</button></p>
             </div>
-        )
-            
+        )            
         }
     }
-
-
-
-
 export default PersonCard;
+
