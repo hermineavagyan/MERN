a6eb2fa344bd928ce229d7ec340cb74b24948593
Hermine <herminemavagyan@gmail.com>
2022-05-23 15:48:07 -0700

    postById added

--- social/nodeapi/controllers/post.js
+++ social/nodeapi/controllers/post.js
@@ -1,7 +1,24 @@
 const Post = require('../models/post');
 const formidable = require('formidable')
-const fs = require('fs')
+const fs = require('fs');
+const _ = require('lodash');
 //let uuidv1 = require('uuidv1')
+
+exports.postById = (req, res, next,id) => {
+    Post.findById(id)
+        .populate("postedBy", "_id name")
+        .exec((err, post) => {
+            if(err || !post){
+                return res.status(400).json({
+                    error: err
+                })
+            }
+        req.post = post
+        next()
+
+    })
+
+}
 
 exports.getPosts = (req, res)=>{
     const posts = Post.find()
@@ -13,6 +30,7 @@ exports.getPosts = (req, res)=>{
     })
     .catch(err => console.log(err))
 };
+
 exports.createPost = (req,res, next)=>{
     const form = new formidable.IncomingForm();
     form.keepExtension = true; 
@@ -42,6 +60,7 @@ exports.createPost = (req,res, next)=>{
         });
     });
 };
+
 exports.postsByUser = (req, res) => {
     Post.find({postedBy: req.profile._id})
         .populate("postedBy", "_id name")
@@ -55,3 +74,41 @@ exports.postsByUser = (req, res) => {
             res.json(posts)
         })
 };
+
+exports.isPoster = (req, res, next) => {
+    let isPoster = req.post && req.auth && req.post.postedBy._id == req.auth._id
+    if(!isPoster){
+        return res.status(403).json({
+            error: "User is not authorized"
+        })
+    }
+    next();
+};
+
+exports.updatePost = (req, res, next) =>{
+    let post = req.post
+    post = _.extend(post,req.body)
+    post.updated = Date.now()
+    post.save( err => {
+        if(err){
+            return res.status(400).json({
+                error: err
+            })
+        }
+        res.json(post);
+    });
+};
+
+exports.deletePost = (req,res) =>{
+    let post = req.post
+    post.remove((err, post) =>{
+        if(err){
+            return res.status(400).json({
+                error: err
+            })
+        }
+        res.json({
+            message: "Post deleted successfully"
+        })
+    })
+}
--- social/nodeapi/routes/post.js
+++ social/nodeapi/routes/post.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {getPosts, createPost, postsByUser} = require('../controllers/post');
+const {getPosts, createPost, postsByUser, postById, isPoster, updatePost, deletePost} = require('../controllers/post');
 const {requireSignin} = require('../controllers/auth');
 const {userById} = require('../controllers/user');
 const {createPostValidator} = require('../validator')
@@ -7,9 +7,17 @@ const {createPostValidator} = require('../validator')
 
 const router = express.Router();
 
-router.get('/',getPosts);
+router.get('/posts',getPosts);
 router.post('/post/new/:userId', requireSignin, createPost, createPostValidator);
-router.get("/posts/by/:userId", requireSignin, postsByUser)
+router.get("/posts/by/:userId", requireSignin, postsByUser);
+router.put('/post/:postId',requireSignin, isPoster, updatePost);
+router.delete('/post/:postId',requireSignin, isPoster, deletePost);
+
+
 //for any route containing :userId, the app will first execute userById()
 router.param("userId", userById)
+
+//for any route containing :postId, the app will first execute postById()
+router.param("postId", postById)
+
 module.exports = router;
