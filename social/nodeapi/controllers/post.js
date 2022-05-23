560e9fd16535ed503e422e29d54ace87d7c666d9
Hermine <herminemavagyan@gmail.com>
2022-05-23 14:05:26 -0700

    getPostsByUser () method and route added

--- social/nodeapi/controllers/post.js
+++ social/nodeapi/controllers/post.js
@@ -4,7 +4,9 @@ const fs = require('fs')
 //let uuidv1 = require('uuidv1')
 
 exports.getPosts = (req, res)=>{
-    const posts = Post.find().select("_id title body")
+    const posts = Post.find()
+    .populate("postedBy", "_id name")
+    .select("_id title body")
     .then((posts)=>{
         res.json({posts})
 
@@ -39,4 +41,17 @@ exports.createPost = (req,res, next)=>{
             res.json(result);
         });
     });
+};
+exports.postsByUser = (req, res) => {
+    Post.find({postedBy: req.profile._id})
+        .populate("postedBy", "_id name")
+        .sort("_created")
+        .exec((err, posts)=>{
+            if(err){
+                return res.status(400).json({
+                    error: err
+                })
+            }
+            res.json(posts)
+        })
 };
