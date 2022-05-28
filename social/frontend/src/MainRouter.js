8d1b3d7e9fbf17e4e0edba8b2b6fed6dc9aeadce
Hermine <herminemavagyan@gmail.com>
2022-05-27 19:48:07 -0700

    update & remove added

--- social/frontend/src/MainRouter.js
+++ social/frontend/src/MainRouter.js
@@ -6,6 +6,7 @@ import Login from './user/Login';
 import MenuBar from './main/MenuBar';
 import Profile from "./user/Profile";
 import Users from './user/Users';
+import EditProfile from './user/EditProfile';
 
 const MainRouter = () => (
     <div>
@@ -15,6 +16,7 @@ const MainRouter = () => (
             <Route exact path = "/users" component={Users}/>
             <Route exact path = "/register" component = {Register}/>
             <Route exact path = "/login" component={Login}/>
+            <Route exact path = "/user/edit/:userId" component={EditProfile}/>
             <Route exact path = "/user/:userId" component={Profile}/>
             
         
--- social/frontend/src/user/getUser.js
+++ social/frontend/src/user/getUser.js
@@ -13,6 +13,21 @@ export const  read = (userId, token) =>{
     .catch (err => console.log(err))
 
 };
+export const  remove = (userId, token) =>{
+    return fetch(`http://localhost:8080/user/${userId}`, {
+        method: "DELETE",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        }
+    })
+    .then(res=> {
+        return res.json()
+    })
+    .catch (err => console.log(err))
+
+};
 export const usersList = () =>{
     return fetch('http://localhost:8080/users', {
         method: "GET",
@@ -22,3 +37,20 @@ export const usersList = () =>{
     })
     .catch (err => console.log(err))
 }
+
+export const  update = (userId, token, user) =>{
+    return fetch(`http://localhost:8080/user/${userId}`, {
+        method: "PUT",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(user)
+    })
+    .then(res=> {
+        return res.json()
+    })
+    .catch (err => console.log(err))
+
+};
