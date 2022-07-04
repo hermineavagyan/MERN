230b0347e56317726b51872827c630cd63b9ea34
Hermine <herminemavagyan@gmail.com>
2022-07-03 19:36:38 -0700

    Optimized complexity

--- JavaScript/Algos/JS/nonRepeatingChar.js
+++ JavaScript/Algos/JS/nonRepeatingChar.js
@@ -1,15 +1,32 @@
-function findNonRepeatingCharacter(string) {
-    let repeat;
-    for (let i = 0; i < string.length; i++) {
-        repeat = false;
-        for (let j = 0; j < string.length; j++) {
-            if (string[i] === string[j] && i !== j) {
-                repeat = true;
-            }
+// function findNonRepeatingCharacter(string) {
+//     let repeat;
+//     for (let i = 0; i < string.length; i++) {
+//         repeat = false;
+//         for (let j = 0; j < string.length; j++) {
+//             if (string[i] === string[j] && i !== j) {
+//                 repeat = true;
+//             }
+//         }
+//         if (repeat === false) {
+//             return i;
+//         }
+//     }
+//     return null;
+// }
+//optimized T = O(n) complexity
+function findNonRepeatingChar(str) {
+    const hashTable = {}
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] in hashTable) {
+            hashTable[str[i]]++;
+        } else {
+            hashTable[str[i]] = 1;
         }
-        if (repeat === false) {
+
+    }
+    for (let i = 0; i < str.length; i++) {
+        if (hashTable[str[i]] === 1)
             return i;
-        }
     }
     return null;
 }
