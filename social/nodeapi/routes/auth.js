84bdca670bbc935705693e79dde9ad9cf2900c93
Hermine <herminemavagyan@gmail.com>
2022-07-07 15:13:37 -0700

    forgetPassword/resetPassword routes

--- social/nodeapi/routes/auth.js
+++ social/nodeapi/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const {signup, signin, signout} = require('../controllers/auth');
-const {userById} = require('../controllers/user');
-const {userSignupValidator} = require('../validator')
+const { signup, signin, signout, forgotPassword, resetPassword } = require('../controllers/auth');
+const { userById } = require('../controllers/user');
+const { userSignupValidator, passwordResetValidator } = require('../validator')
 
 
 const router = express.Router();
@@ -9,6 +9,10 @@ const router = express.Router();
 router.post('/signup', userSignupValidator, signup);
 router.post('/signin', signin);
 router.get('/signout', signout);
+
+// password forgot and reset routes
+router.put('/forgot-password', forgotPassword);
+router.put('/reset-password', passwordResetValidator, resetPassword);
 
 //for any route containing :userId, the app will first execute userById()
 router.param("userId", userById)
